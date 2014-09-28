e0c7ace1f27cd71068672a07be0602f70d180f54
Krzysztof Voss <kmv633@mail.usask.ca>
2014-09-28 13:02:34 -0600

    using internal dates

--- p12/clinic/views.py
+++ p12/clinic/views.py
@@ -1,10 +1,31 @@
+from datetime import timedelta, time, datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from django.utils import timezone
 
 from clinic.models import Clinic
 
 def index(req):
-    ctx = { 'clinics': Clinic.objects.all() }
+    clinics = Clinic.objects.all()
+
+    2h = timedelta(hours=2)
+
+    for cl in clinics:
+        datetime(cl.last_update)
+        dt = timedelta(minutes=cl.est_wait_min)
+
+        datetime.now()
+
+        cl.waiting = 40
+
+
+
+    ctx = { 'clinics': clinics,
+    }
+
+
     return render(req, 'clinic/hello.html', ctx)
 
 
