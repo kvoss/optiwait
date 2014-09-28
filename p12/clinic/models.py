52c148ef2f021c7c39ab165c0df376af93102cbd
Krzysztof Voss <kmv633@mail.usask.ca>
2014-09-27 19:12:30 -0600

    open hours issue

--- p12/clinic/models.py
+++ p12/clinic/models.py
@@ -2,13 +2,21 @@ from django.db import models
 
 # Create your models here.
 
-
 class Clinic(models.Model):
     name = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    twitter = models.CharField(max_length=100, default='')
+    location = models.CharField(max_length=100)
+    twitter = models.CharField(max_length=100, blank=True)
+    phone = models.CharField(max_length=100, blank=True)
+    notes = models.CharField(max_length=1000, blank=True)
+
+    # adding additional many-to-many realationship with working hours
+    # for now logic assumer 9-5
+
 
 class Doctor(models.Model):
     name = models.CharField(max_length=100)
+    notes = models.CharField(max_length=1000, blank=True)
+
     clinic = models.ForeignKey(Clinic)
+
 
