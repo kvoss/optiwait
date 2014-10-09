e2cbd9cb854558424648d1f55fcf5c62b6f90477
Krzysztof Voss <kmv633@mail.usask.ca>
2014-10-09 17:10:39 -0600

    better code organization

--- p12/clinic/tweets.py
+++ p12/clinic/tweets.py
@@ -1,5 +1,8 @@
-import twitter
 import json
+import re
+
+import twitter
+from dateutil.parser import parse
 
 # XXX: Go to http://dev.twitter.com/apps/new to create an app and get values   
 # for these credentials, which you'll need to provide in place of these           
@@ -7,20 +10,55 @@ import json
 # See https://dev.twitter.com/docs/auth/oauth for more information                
 # on Twitter's OAuth implementation.                                              
 
+class TweetError(Exception):
+    pass
+
 CONSUMER_KEY = 'lMk5wnSsEqRFqB6wTyQ00TlLk'
 CONSUMER_SECRET ='A0XRy7zkgFej4fnTNqbKLF4QOhIRU6ygIul00JVYGE84BpPsO5'
 OAUTH_TOKEN = '2835226370-1kIFZSqIsw9obeEFPGlkFhj3vwpSLna4aVcrCYK'
 OAUTH_TOKEN_SECRET = 'NfAQTO3S2bQV4t95L6u3L71oZmtbhzjcfT9BXbgsjQ1fZ'
 
+PATTERN = re.compile(r'(\d+) [Mm]in')
+PATTERNH = re.compile(r'(\d+) [Hh]our')
+
 auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
                            CONSUMER_KEY, CONSUMER_SECRET)
-
 twitter_api = twitter.Twitter(auth=auth)
 
 def get_msgs(clinic_id):
-    tt =  twitter_api.statuses.user_timeline(screen_name=clinic_id)
-    for msg in tt:
-        print json.dumps(msg['text'], indent=1)
+    #tt =  twitter_api.statuses.user_timeline(screen_name=clinic_id)
+    #for msg in tt:
+    #    print json.dumps(msg['text'], indent=1)
 
-# get_msgs('LMCSaskatoon')
+    tts =  twitter_api.statuses.user_timeline(screen_name=clinic_id)
+    msg = tts[0]
+    dt = parse(msg['created_at'])
+
+    #ret = {'update' : dt, 'msg': msg['text'] }
+    ret = (dt, msg['text'])
+
+    return ret
+
+
+def get_update(tt_id):
+    if tt_id == '':
+        raise TweetError
+    else:
+        t, txt = get_msgs(tt_id)
+
+    sre = PATTERN.search(txt)
+    sreh = PATTERNH.search(txt)
+    #print txt
+    if sre:
+        mins = sre.groups(1)
+        mins = int( mins[0])
+    elif sreh:
+        hours = sreh.groups(1)
+        mins = int(hours[0]) * 60
+    else:
+        mins = 60
+
+    return (t, mins)
+
+#print get_update('LMCSaskatoon')
 
--- p12/clinic/views.py
+++ p12/clinic/views.py
@@ -1,86 +1,36 @@
 from datetime import timedelta, time, datetime
-from dateutil.parser import parse
 from django.utils import timezone
 
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from clinic.models import Clinic
-
-import twitter
-
-import re
-
-class TweetError(Exception):
-    pass
-
-PATTERN = re.compile(r'(\d+) [Mm]in')
-PATTERNH = re.compile(r'(\d+) [Hh]our')
-
-CONSUMER_KEY = 'lMk5wnSsEqRFqB6wTyQ00TlLk'
-CONSUMER_SECRET ='A0XRy7zkgFej4fnTNqbKLF4QOhIRU6ygIul00JVYGE84BpPsO5'
-OAUTH_TOKEN = '2835226370-1kIFZSqIsw9obeEFPGlkFhj3vwpSLna4aVcrCYK'
-OAUTH_TOKEN_SECRET = 'NfAQTO3S2bQV4t95L6u3L71oZmtbhzjcfT9BXbgsjQ1fZ'
-
-auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
-                           CONSUMER_KEY, CONSUMER_SECRET)
-
-twitter_api = twitter.Twitter(auth=auth)
-
-def get_msgs(clinic_id):
-    tts =  twitter_api.statuses.user_timeline(screen_name=clinic_id)
-    msg = tts[0]
-    dt = parse(msg['created_at'])
-
-    ret = {'update' : dt, 'msg': msg['text'] }
-
-    return ret
-
-
-def get_update(tt_id):
-    if tt_id == '':
-        raise TweetError
-    else:
-        ret = get_msgs(tt_id)
-    return (ret['update'], ret['msg'])
-
+from clinic.tweets import get_update, TweetError
 
 HOUR = timedelta(hours=1)
 
 def index(req):
+
+    now = timezone.now()
+
     clinics = Clinic.objects.all()
-
     for cl in clinics:
+        dt = timedelta(minutes=cl.est_wait_min)
+        lu = cl.last_update
+
         try:
-            t, txt = get_update(cl.twitter)
+            t, mins = get_update(cl.twitter)
 
-            sre = PATTERN.search(txt)
-            sreh = PATTERNH.search(txt)
-            #print txt
-            if sre:
-                mins = sre.groups(1)
-                mins = int( mins[0])
-            elif sreh:
-                hours = sreh.groups(1)
-                mins = int(hours[0]) * 60
-            else:
-                mins = 60
+            print '[!]>> ', cl.name, cl.last_update, t
 
-            print '[!]>> ', cl.name, t
-            print '[!]>> ', cl.name, cl.last_update
-
-            if t >= cl.last_update:
+            if t > lu:
                 # updating the DB
                 cl.last_update = t
                 cl.est_wait_min = mins
                 cl.save()
+                lu = cl.last_update
         except TweetError:
             pass
-
-        dt = timedelta(minutes=cl.est_wait_min)
-        lu = cl.last_update
-
-        now = timezone.now()
 
         # if no update in the past 8 hours
         #  assumes a clinic works 8 hours and posted update first time in the
@@ -91,8 +41,7 @@ def index(req):
         else:
             cl.waiting = int(dt.seconds) / 60
 
-    ctx = { 'clinics': sorted(clinics, key=lambda x: x.waiting),
-    }
+    ctx = { 'clinics': sorted(clinics, key=lambda x: x.waiting), }
 
     return render(req, 'clinic/hello.html', ctx)
 
